5d7e863d860aea424fb317abb572394a45a0dfc2
jassbot232 <jashanbeniwal2006@gmail.com>
2025-09-23 13:42:19 +0530

    Update settings.py

--- modules/settings.py
+++ modules/settings.py
@@ -39,7 +39,7 @@ def register_settings_handlers(bot):
         ])
         await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://ibb.co/dJztbctg",
           caption=caption
         ),
         reply_markup=keyboard
@@ -56,7 +56,7 @@ def register_settings_handlers(bot):
         ])
         await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://ibb.co/9kYbQPPd",
           caption=caption
         ),
         reply_markup=keyboard
@@ -74,7 +74,7 @@ def register_settings_handlers(bot):
         ])
         await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://ibb.co/9kYbQPPd",
           caption=caption
         ),
         reply_markup=keyboard
